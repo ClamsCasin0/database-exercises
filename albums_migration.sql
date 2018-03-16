7a9ae9fd4adf74634ce675cb49e377954fe4343c
armandosegura <segura_arm@yahoo.com>
2018-03-16 10:58:32 -0500

    made my first DB! Z

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,13 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(25),
+  name  VARCHAR(50),
+  release_date INT(4),
+  sales FLOAT(12),
+  genre VARCHAR(15),
+  PRIMARY KEY (id)
+);
