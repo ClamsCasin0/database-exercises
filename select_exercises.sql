cb23458c0115a451ee55eb49dbd0c13a31eec52a
armandosegura <segura_arm@yahoo.com>
2018-03-19 12:18:05 -0500

    added employees db, need some lunch

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,12 +2,12 @@ USE codeup_test_db;
 
 SELECT name, 'Yes' AS 'Is an album by Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT release_date, '1967' AS 'The year the Beatles released Sgt. Pepper''s' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT artist, name, release_date, '1967' AS 'The year the Beatles released Sgt. Pepper''s' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT genre, 'Grunge, Alt rock' AS 'The genre for Nirvana''s ''Nevermind''' FROM albums WHERE name = 'Nevermind';
 
-SELECT name, artist, 'Yes' AS 'Albums released after 1990' FROM albums WHERE release_date >= '1990';
+SELECT name, artist, release_date, 'Yes' AS 'Was the album released during the ''90''s' FROM albums WHERE release_date BETWEEN '1990' and '1999';
 
-SELECT name, artist, 'Yes' AS 'Did the album have less than 20 million sales' FROM albums WHERE sales <= '20000000';
+SELECT artist, name, sales, 'Yes' AS 'Did the album have less than 20 million sales' FROM albums WHERE sales <= '20000000';
 
-SELECT name, artist, 'Yes' As 'Has the genre ''Rock''' FROM albums WHERE genre = 'Rock';
+SELECT artist, name, genre, 'Yes' As 'Falls into the genre ''Rock''' FROM albums WHERE genre LIKE '%Rock%';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,26 +1,39 @@
 USE codeup_test_db;
+
+
 #  1/a
 SELECT * FROM albums;
+
 # 1/b
 UPDATE albums
 SET sales = sales * 10;
+
 # 1/c
 SELECT sales FROM albums;
 
+# ----------------------------------------------------------
+
 # 2/a
-SELECT *, 'Yes' AS 'Was the Album released before 1980?' FROM albums WHERE release_date <= '1980';
+SELECT *, 'Yes' AS 'Was the Album released before 1980?' FROM albums WHERE release_date < '1980';
+
 # 2/b
 UPDATE albums
-SET release_date = '1800'
-WHERE release_date <= '1980';
+SET release_date = release_date - 100
+WHERE release_date < '1980';
+
 # 2/c
 SELECT artist, name, release_date FROM albums WHERE release_date = '1800';
 
+# ------------------------------------------------------------
+
 # 3/a
-SELECT *, 'Yes' AS 'Did Micheal Jackson make this?' FROM albums WHERE artist = 'Micheal Jackson';
+SELECT artist, name, 'Yes' AS 'Did Micheal Jackson make this?' FROM albums WHERE artist = 'Micheal Jackson';
+
 # 3/b
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Micheal Jackson';
+
 # 3/c
 SELECT * FROM albums WHERE artist = 'Peter Jackson';
+
