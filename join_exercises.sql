8e4603c46478ac1a23a5fa3be0c428a95a060163
armandosegura <segura_arm@yahoo.com>
2018-03-21 12:21:23 -0500

    still working on bonuse, finished sub-queries exercise, LUNCHTIME

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,8 +1,7 @@
-
 USE employees;
 
 
-# Base Example
+# Helper queries
 DESCRIBE dept_manager;
 SELECT * FROM departments;
 SELECT * FROM dept_emp;
@@ -11,69 +10,84 @@ SELECT * FROM titles;
 SELECT * FROM employees;
 SELECT * FROM salaries;
 # Base example
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+  d.dept_name
 FROM employees as e
-  JOIN dept_emp as de
-    ON de.emp_no = e.emp_no
-  JOIN departments as d
-    ON d.dept_no = de.dept_no
+  JOIN dept_emp as de ON de.emp_no = e.emp_no
+  JOIN departments as d ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
 # 2
 SELECT d.dept_name AS 'Department Name',
-CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+  CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees AS e
-JOIN dept_manager AS de ON  de.emp_no = e.emp_no
-JOIN departments AS d ON  d.dept_no = de.dept_no
+  JOIN dept_manager AS de ON  de.emp_no = e.emp_no
+  JOIN departments AS d ON  d.dept_no = de.dept_no
 WHERE de.to_date >= CURDATE()
 ORDER BY d.dept_name;
 
 # 3
 SELECT d.dept_name AS 'Department Name',
-CONCAT(e.first_name, ' ', e.last_name) AS 'Manager Name'
+  CONCAT(e.first_name, ' ', e.last_name) AS 'Manager Name'
 FROM employees AS e
-JOIN dept_manager AS de ON  de.emp_no = e.emp_no
-JOIN departments AS d ON  d.dept_no = de.dept_no
+  JOIN dept_manager AS de ON  de.emp_no = e.emp_no
+  JOIN departments AS d ON  d.dept_no = de.dept_no
 WHERE de.to_date >= CURDATE()
-AND gender = 'F'
+  AND gender = 'F'
 ORDER BY d.dept_name;
 
 # 4 d009 customer service
 SELECT t.titles, COUNT(e.emp_no)
 FROM titles AS t
-JOIN employees AS e ON e.emp_no = t.emp_no
-JOIN dept_emp AS de ON de.emp_no = e.emp_no
-JOIN departments AS d ON d.dept_no = de.dept_no
+  JOIN employees AS e ON e.emp_no = t.emp_no
+  JOIN dept_emp AS de ON de.emp_no = e.emp_no
+  JOIN departments AS d ON d.dept_no = de.dept_no
 WHERE t.to_date >= CURDATE()
-AND d.dept_name = 'Customer Service'
-AND de.to_date >= CURDATE()
+  AND d.dept_name = 'Customer Service'
+  AND de.to_date >= CURDATE()
 GROUP BY t.title;
 
 # 5
 SELECT d.dept_name AS 'Department Name',
-CONCAT(e.first_name, ' ', e.last_name) AS 'Name',
-s.salary AS 'Salary'
+  CONCAT(e.first_name, ' ', e.last_name) AS 'Name',
+  s.salary AS 'Salary'
 FROM employees AS e
-JOIN dept_manager AS dm ON  dm.emp_no = e.emp_no
-JOIN departments AS d ON  d.dept_no = dm.dept_no
-JOIN salaries AS s ON s.emp_no = e.emp_no
+  JOIN dept_manager AS dm ON  dm.emp_no = e.emp_no
+  JOIN departments AS d ON  d.dept_no = dm.dept_no
+  JOIN salaries AS s ON s.emp_no = e.emp_no
 WHERE dm.to_date >= CURDATE()
-AND s.to_date >= CURDATE()
+  AND s.to_date >= CURDATE()
 ORDER BY d.dept_name;
 
 # Bonus
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
-d.dept_name AS 'Department Name',
-CONCAT(emp.first_name, ' ', emp.last_name) AS 'Manager Name'
+  d.dept_name AS 'Department Name',
+  CONCAT(emp.first_name, ' ', emp.last_name) AS 'Manager Name'
 FROM employees AS e
-JOIN dept_emp AS de ON e.emp_no = de.emp_no
-JOIN departments AS d ON de.dept_no = d.dept_no
-JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
-JOIN employees AS emp ON dm.emp_no = emp.emp_no
+  JOIN dept_emp AS de ON e.emp_no = de.emp_no
+  JOIN departments AS d ON de.dept_no = d.dept_no
+  JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+  JOIN employees AS emp ON dm.emp_no = emp.emp_no
 WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
 
+# Justin's extra bonus
+# Write a query to get the average salary current managers
+# make by gender
+# M 79350.60
+# F 75690.00
 
+SELECT AVG(salary), gender
+FROM employees AS e
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
 
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+)
+LIMIT 10;
 
---
+++ subqueries_exercises.sql
@@ -0,0 +1,53 @@
+USE employees;
+
+
+# base example
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+)
+LIMIT 10;
+
+# 1
+SELECT *
+FROM employees AS e
+WHERE e.hire_date IN (
+  SELECT emp.hire_date
+  FROM employees AS emp
+  WHERE emp.emp_no = 101010);
+
+# 2
+SELECT *
+FROM titles AS t
+WHERE t.emp_no IN (
+  SELECT e.emp_no
+  FROM employees AS e
+  WHERE e.first_name = 'Aamod')
+GROUP BY t.title;
+
+# 3
+SELECT first_name, last_name
+FROM employees AS e
+WHERE e.emp_no IN (
+  SELECT dm.emp_no
+  FROM dept_manager AS dm
+  WHERE dm.to_date = '9999-01-01')
+AND e.gender = 'F';
+
+# BONUS
+
+SELECT d.dept_name
+FROM departments AS d
+WHERE d.dept_no IN (
+  SELECT dm.dept_no
+  FROM dept_manager AS dm
+  WHERE dm.to_date = '9999-01-01'
+    AND dm.emp_no IN (
+      SELECT e.emp_no
+      FROM employees AS e
+      WHERE e.gender = 'F'
+    )
+  )
+ORDER BY d.dept_name;
